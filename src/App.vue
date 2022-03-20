e4dd33f36ed67ae85e1bd4c980e5f218f145b993
zyryanov_ivan <89224747505@mail.ru>
2022-03-20 17:45:06 +0500

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <label>Text: </label>
+  <input @keydown.enter="addTicker" v-model="ticker" type="text"/>
+  <button @click="addTicker" class="btn">Добавить</button>
+  <hr>
+  <div class="wrapper">
+    <div class="ticketMessage" v-for="item in tickers" :key="item.id">
+      <div class="ticketMessage__name">{{ item.name }}</div>
+      <div class="ticketMessage__price">
+        <div class="tMessPrice__label">Цена валюты</div>
+        <br>
+        <div class="tMessPrice__value">{{ item.price }}</div>
+      </div>
+      <button @click="deleteItem(item)">Удалить</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      ticker: "",
+      tickers: [],
+      index:0,
+    }
+  },
+  methods: {
+    addTicker() {
+      if (this.ticker.length > 4) this.ticker=this.ticker.slice(0,3);
+      if (this.ticker == "") return
+      const tickerObjForArray = {id:this.index, name: this.ticker.toUpperCase()+'-USD', price: '49560.12'}
+      this.index++;
+      this.tickers.push(tickerObjForArray);
+      this.ticker = "";
+    },
+    deleteItem(ticketItem) {
+      this.tickers = this.tickers.filter(value => value.id !== ticketItem.id);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.btn {
+  margin-left: 50px;
+}
+
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.ticketMessage {
+  border: 1px solid #000;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.ticketMessage__name {
+  color: brown;
+  font-size: 30px;
+  font-weight: 700;
+  padding-bottom: 30px;
+}
+.ticketMessage__price {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.tMessPrice__label {
+  color: black;
+  font-size: 20px;
+  font-weight: 900;
+}
+.tMessPrice__value {
+  padding-bottom: 20px;
+  font-size: 36px;
+  font-weight: 900;
 }
 </style>
